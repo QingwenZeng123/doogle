77531804975498a205b501ef308b1c33752b41a4
Kevin Perez <kperez29@lion.lmu.edu>
2022-11-28 11:37:19 -0800

    Added components to top bar

--- src/topbar/TopBar.js
+++ src/topbar/TopBar.js
@@ -1,13 +1,15 @@
-import "./topbar.css"
+import "./topbar.css";
 
 export default function TopBar() {
   return (
-    <div className='topbar'>
-        <div className="home">home</div>
-        <div className="play">play</div>
-        <div className="services">services</div>
-        <div className="local__breeders">breeders</div>
-        <div className="contact__us">contact</div>
+    <div className="topbar">
+      <div className="home">Home</div>
+      <div className="play">Play</div>
+      <div className="services">Services</div>
+      <div className="local__breeders">Breeders</div>
+      <div className="contact__us">Contact Us</div>
+      <div className="sign__in">Sign In</div>
+      <div className="sign__up">Sign Up</div>
     </div>
-  )
+  );
 }
