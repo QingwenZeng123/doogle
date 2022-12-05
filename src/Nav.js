02ce695a3e80caffad9d98e26483956e5b4de72f
Kevin Perez <kperez29@lion.lmu.edu>
2022-12-05 11:29:54 -0800

    Added Home page to nav bar

--- src/Nav.js
+++ src/Nav.js
@@ -1,19 +1,20 @@
-import './topbar.css'
-import './App.css'
+import "./topbar.css";
+import "./App.css";
 
 export default function Nav({ setPage }) {
   return (
     <div class="topnav">
-      <div onClick={() => setPage('parks')}>Parks</div>
-      <div onClick={() => setPage('entertainment')}>Entertainment</div>
-      <div onClick={() => setPage('events')}>Events</div>
-      <div onClick={() => setPage('contact')}>Services</div>
-      <div onClick={() => setPage('about')}>Breeders</div>
-      <div onClick={() => setPage('contact')}>Contact</div>
-      <div onClick={() => setPage('signin')}>Sign In</div>
-      <div onClick={() => setPage('signup')}>Sign up</div>
+      <div onClick={() => setPage("Home")}>Home</div>
+      <div onClick={() => setPage("parks")}>Parks</div>
+      <div onClick={() => setPage("entertainment")}>Entertainment</div>
+      <div onClick={() => setPage("events")}>Events</div>
+      <div onClick={() => setPage("contact")}>Services</div>
+      <div onClick={() => setPage("about")}>Breeders</div>
+      <div onClick={() => setPage("contact")}>Contact</div>
+      <div onClick={() => setPage("signin")}>Sign In</div>
+      <div onClick={() => setPage("signup")}>Sign up</div>
     </div>
-  )
+  );
 }
 
 //      /*park, local breeder in services*/
