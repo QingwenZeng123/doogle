9663cb67dbdd3a28dfdab1bf019ccb4b2a705054
dhuang2130 <dhuang11@lion.lmu.edu>
2022-12-08 13:01:11 -0800

    more formatting of home page

--- src/Nav.js
+++ src/Nav.js
@@ -1,21 +1,21 @@
-import "./topbar.css";
-import "./App.css";
+import './topbar.css'
+import './App.css'
 
 export default function Nav({ setPage }) {
   return (
     <div class="topnav">
       <divA>Doggle</divA>
-      <div onClick={() => setPage("Home")}>Home</div>
-      <div onClick={() => setPage("parks")}>Parks</div>
-      <div onClick={() => setPage("entertainment")}>Entertainment</div>
-      <div onClick={() => setPage("events")}>Events</div>
-      <div onClick={() => setPage("contact")}>Services</div>
-      <div onClick={() => setPage("about")}>Breeders</div>
-      <div onClick={() => setPage("contact")}>Contact</div>
-      <div onClick={() => setPage("signin")}>Sign In</div>
-      <div onClick={() => setPage("signup")}>Sign up</div>
+      <div onClick={() => setPage('signup')}>Sign up</div>
+      <div onClick={() => setPage('signin')}>Sign In</div>
+      <div onClick={() => setPage('contact')}>Contact</div>
+      <div onClick={() => setPage('about')}>Breeders</div>
+      <div onClick={() => setPage('contact')}>Services</div>
+      <div onClick={() => setPage('events')}>Events</div>
+      <div onClick={() => setPage('entertainment')}>Entertainment</div>
+      <div onClick={() => setPage('parks')}>Parks</div>
+      <div onClick={() => setPage('Home')}>Home</div>
     </div>
-  );
+  )
 }
 
 //      /*park, local breeder in services*/
