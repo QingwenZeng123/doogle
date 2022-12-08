fd9a5931efc775eb66bafda5a42dda418bfd5e47
dhuang2130 <dhuang11@lion.lmu.edu>
2022-12-08 01:49:26 -0800

    added some formatting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,18 +3,20 @@ import React from "react";
 
 export default function Home() {
   return <div className="home">
-    <div className="Events">
-        <page id="Events">Coming Events</page>
-      <div className="EventsLearn">
-        <page id="Events">Learn More</page>
+    <div className="Box">
+      <div className="EventBox">
+        <page>Coming Events</page>
+        <page>Learn More</page>
       </div>
+      
+      <div className="EventBox">
+        <page>Parks</page>
+        <page>Learn More</page>
       </div>
-      <div className="Parks">
-        <page id="Parks">Parks</page>
-      <div className="Services">
-        <page id="Services">Services</page>
-        </div>
-      </div>
-      </div>
-
+    </div>
+    <div className="ServicesBox">
+      <page>Services</page>
+      <page>Learn More</page>
+    </div>
+  </div>
 }
