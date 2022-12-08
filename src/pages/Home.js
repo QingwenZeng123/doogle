94d89ad46a72e2f1ede5f72ebe1c71d050039393
dhuang2130 <dhuang11@lion.lmu.edu>
2022-12-08 12:58:52 -0800

    more formatting of home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,35 @@
-import React from "react";
-
+import React from 'react'
 
 export default function Home() {
-  return <div className="home">
-    <div className="Box">
-      <div className="EventBox">
-        <page>Coming Events</page>
-        <page>Learn More</page>
+  return (
+    <div className="home">
+      <div className="Box">
+        <div className="EventBox">
+          <page>Coming Events</page>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <page>Learn More</page>
+        </div>
+
+        <div className="EventBox">
+          <page>Parks</page>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <page>Learn More</page>
+        </div>
       </div>
-      
-      <div className="EventBox">
-        <page>Parks</page>
+      <div className="ServicesBox">
+        <page>Services</page>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
         <page>Learn More</page>
       </div>
     </div>
-    <div className="ServicesBox">
-      <page>Services</page>
-      <page>Learn More</page>
-    </div>
-  </div>
+  )
 }
