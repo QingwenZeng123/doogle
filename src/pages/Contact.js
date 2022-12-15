c721d8ab6deae02b193d2c59a181857b7e098d7b
Ngan Tran <ngan170904@gmail.com>
2022-12-15 00:26:42 -0800

    Updating

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 export default function Contact() {
-  return <div className="contact-container">
-    <div>Contact Us</div>
-    <li>111-111-1111</li>
-              <li>LMU Drive</li>
-              <li>doogle@lion.lmu.edu</li>
-  </div>;
+  return (
+    <div className="contact-container">
+      {/* <div>Contact Us</div>
+      <li>111-111-1111</li>
+      <li>LMU Drive</li>
+      <li>doogle@lion.lmu.edu</li> */}
+      <form>
+        
+      </form>
+    </div>
+  );
 }
