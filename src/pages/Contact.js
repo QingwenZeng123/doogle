17dd8b26b6e2099d1a4ea3d5effd1a574431d042
Ngan Tran <ngan170904@gmail.com>
2022-12-15 01:16:18 -0800

    Adding contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,12 +3,47 @@ import React from "react";
 export default function Contact() {
   return (
     <div className="contact-container">
-      {/* <div>Contact Us</div>
-      <li>111-111-1111</li>
-      <li>LMU Drive</li>
-      <li>doogle@lion.lmu.edu</li> */}
       <form>
-        
+        <h2>We'd Love To Hear From You</h2>
+        <p>
+          Whether you're concern about features, services, location - We're
+          ready to answer any question
+        </p>
+        <div className="input-container">
+          <label>Name</label>
+          <input
+            type="text"
+            className="input-field"
+            placeholder="Enter your name"
+          />
+        </div>
+        <div className="input-container">
+          <label>Email</label>
+          <input
+            type="text"
+            className="input-field"
+            placeholder="Enter your email"
+          />
+        </div>
+        <div className="input-container">
+          <label>Subject</label>
+          <input
+            type="text"
+            className="input-field"
+            placeholder="Enter your subject"
+          />
+        </div>
+        <div className="input-container">
+          <label>Message</label>
+          <textarea
+            type="text"
+            className="input-field textarea-field"
+            placeholder="Enter your message"
+          />
+        </div>
+        <button type="button" className="submit">
+          Send
+        </button>
       </form>
     </div>
   );
