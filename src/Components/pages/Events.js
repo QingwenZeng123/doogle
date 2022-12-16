ddff3b07a077aa7df03c5a97df3faa79892054ba
QingwenZeng123 <qzeng@lion.lmu.edu>
2022-12-16 16:16:18 -0500

    addEvents

--- src/Components/pages/Events.js
+++ src/Components/pages/Events.js
@@ -1,5 +1,40 @@
 import React from "react";
 
-export default function Events() {
-  return <div>Event</div>;
+export default function Events(props) {
+  console.log(props);
+
+  return (
+    <div className="Box-container">
+      <div className="Box-container-row">
+        <div className="EventsBox-Events-page">
+          <h1>Christams</h1>
+          <h1>Date</h1>
+        </div>
+        <div className="EventsBox-Events-page">
+          <h1>Happy New Year</h1>
+          <h1>Date</h1>
+        </div>
+      </div>
+      <div className="Box-container-row">
+        <div className="EventsBox-Events-page">
+          <h1>Cute Puppies</h1>
+          <h1>Date</h1>
+        </div>
+        <div className="EventsBox-Events-page">
+          <h1>Playground</h1>
+          <h1>Date</h1>
+        </div>
+      </div>
+      <div className="Box-container-row">
+        <div className="EventsBox-Events-page">
+          <h1>Costume Ball</h1>
+          <h1>Date</h1>
+        </div>
+        <div className="EventsBox-Events-page">
+          <h1>Free Lunch</h1>
+          <h1>Date</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
