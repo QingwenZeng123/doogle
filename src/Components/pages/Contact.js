33bcc51e8f81c44d459d3e2cdfa79f074cc6f3b0
Kevin Perez <kperez29@lion.lmu.edu>
2022-12-19 21:41:28 -0800

    update working contact form to database

--- src/Components/pages/Contact.js
+++ src/Components/pages/Contact.js
@@ -1,63 +1,74 @@
 import React from "react";
-import { db } from "../../firebaseConfig";
-import { collection, addDoc, Timestamp } from "firebase/firestore";
-
-export async function createContact({ name, email, message, subject }) {
-  const data = { name, email, message, subject, date: Timestamp.now() };
-  const docRef = await addDoc(collection(db, "contactUs"), data);
-  return { id: docRef.id, ...data };
-}
+import { useState } from "react";
+import { createContact } from "../../services/contactService";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState(null);
+
+  function submit(e) {
+    setError(null);
+    e.preventDefault();
+    if (!name.trim() || !email.trim() || !subject.trim() || !message.trim()) {
+      setError("Information must be supplied");
+    } else {
+      createContact({ name, email, subject, message });
+    }
+  }
+
   return (
     <div className="contact-container">
-      <form>
+      <form onSubmit={submit}>
+        {error && <p className="error">{error}</p>}
         <h2>We'd Love To Hear From You</h2>
         <p>
           Whether you are concerned about features, services or locations - We
           are ready to answer any questions!
         </p>
         <div className="input-container">
-          <label id="name" for="userName">
-            Name
-          </label>
+          <label>Name</label>
           <input
             type="text"
             className="input-field"
             placeholder="Enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div className="input-container">
-          <label id="email" for="userEmail">
-            Email
-          </label>
+          <label>Email</label>
           <input
             type="text"
             className="input-field"
             placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="input-container">
-          <label id="subject" for="userSubject">
-            Subject
-          </label>
+          <label>Subject</label>
           <input
             type="text"
             className="input-field"
             placeholder="Enter your subject"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
           />
         </div>
         <div className="input-container">
-          <label id="mesage" for="userMessage">
-            Message
-          </label>
+          <label>Message</label>
           <textarea
             type="text"
             className="input-field textarea-field"
             placeholder="Enter your message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           />
         </div>
-        <button type="button" className="submit">
+        <button type="submit" className="submit">
           Send
         </button>
       </form>
