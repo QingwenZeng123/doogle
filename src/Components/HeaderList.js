03c5041b2fabdb0191f200aa7e5a03172f990733
QingwenZeng123 <qzeng@lion.lmu.edu>
2022-12-20 00:57:00 -0500

    responsiveness

--- src/Components/HeaderList.js
+++ src/Components/HeaderList.js
@@ -1,9 +1,9 @@
 import React from "react";
 import HomeBG from "../background/HomeBG.jpg";
-import ContactBG from "../background/ContactBG.webp";
-import EventsBG from "../background/EventsBG.webp";
-import ShopsBG from "../background/ShopsBG.jpg";
-import ParksBG from "../background/ParksBG.jpg";
+import ContactBG from "../background/contactBG.webp";
+import EventsBG from "../background/eventsBG.webp";
+import ShopsBG from "../background/shopsBG.jpg";
+import ParksBG from "../background/parksBG.jpg";
 
 const HeaderList = {
   Home: {
