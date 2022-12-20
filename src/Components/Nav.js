c5b323575c24c0183b6565ba108b929741ef98a1
Ngan Tran <ngan170904@gmail.com>
2022-12-20 13:25:51 +0700

    Updating

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,26 +4,24 @@ export default function Nav({ setPage }) {
   const user = useAuthentication();
 
   return (
-    <div className="topnav">
-      <div className="topnav-page-title-container">
-        <h1 className="web-name">Doogle</h1>
-        <div className="topnav-page-container">
-          <button className="topnav-style" onClick={() => setPage("Home")}>
-            Home
-          </button>
-          <button className="topnav-style" onClick={() => setPage("Parks")}>
-            Parks
-          </button>
-          <button className="topnav-style" onClick={() => setPage("Events")}>
-            Events
-          </button>
-          <button className="topnav-style" onClick={() => setPage("Shops")}>
-            Shops
-          </button>
-          <button className="topnav-style" onClick={() => setPage("Contact")}>
-            Contact
-          </button>
-        </div>
+    <div className="nav__container">
+      <h1 className="nav__web-name">Doogle</h1>
+      <div className="nav__page-container">
+        <button className="nav__page" onClick={() => setPage("Home")}>
+          Home
+        </button>
+        <button className="nav__page" onClick={() => setPage("Parks")}>
+          Parks
+        </button>
+        <button className="nav__page" onClick={() => setPage("Events")}>
+          Events
+        </button>
+        <button className="nav__page" onClick={() => setPage("Shops")}>
+          Shops
+        </button>
+        <button className="nav__page" onClick={() => setPage("Contact")}>
+          Contact
+        </button>
       </div>
       {!user ? <SignIn /> : <SignOut />}
     </div>
