6d9ee0512d07c249bea9255a0cfe9dfd03e7713a
Ngan Tran <ngan170904@gmail.com>
2022-12-20 10:03:05 +0700

    Updating css from Maika

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,7 +4,7 @@ export default function Nav({ setPage }) {
   const user = useAuthentication();
 
   return (
-    <div className="topnav flex">
+    <div className="topnav">
       <div className="topnav-page-title-container">
         <h1 className="web-name">Doogle</h1>
         <div className="topnav-page-container">
